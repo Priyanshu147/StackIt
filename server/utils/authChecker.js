849eac0baba2eff77be50217f81ed735bd5e9a6c
Pranay Patel <124255411+Pranay8282@users.noreply.github.com>
2025-07-12 17:00:02 +0530

    Fixed bug in utils

--- server/utils/authChecker.js
+++ server/utils/authChecker.js
@@ -1,19 +1,19 @@
-const { AuthenticationError } = require('apollo-server');
-const jwt = require('jsonwebtoken');
-const { SECRET } = require('./config');
+  const { AuthenticationError } = require('apollo-server');
+  const jwt = require('jsonwebtoken');
+  const { SECRET } = require('./config');
 
-const authChecker = (context) => {
-  const token = context.req.headers.authorization;
-  if (!token) {
-    throw new AuthenticationError('No auth token found. Authorization denied.');
-  }
+  const authChecker = (context) => {
+    const token = context.req.headers.authorization;
+    if (!token) {
+      throw new AuthenticationError('No auth token found. Authorization denied.');
+    }
 
-  try {
-    const decodedUser = jwt.verify(token, SECRET);
-    return decodedUser;
-  } catch (err) {
-    throw new AuthenticationError(err);
-  }
-};
+    try {
+      const decodedUser = jwt.verify(token, SECRET);
+      return decodedUser;
+    } catch (err) {
+      throw new AuthenticationError(err);
+    }
+  };
 
-module.exports = authChecker;
+  module.exports = authChecker;
